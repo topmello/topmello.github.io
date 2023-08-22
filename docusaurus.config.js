02ff09e484dfb6c161fd1438b502b37769992221
cam <cwan0148@student.monash.edu>
2023-08-22 10:24:58 +1000

    update docusaurus deployment config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'topmello', // Usually your GitHub org/user name.
+  projectName: 'topmello.github.io', // Usually your repo name.
+  deploymentBranch: 'deployment',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
