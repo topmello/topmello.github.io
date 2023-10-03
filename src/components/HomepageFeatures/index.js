d3107d7b4bbb8e90cc9e47d9890aef593036ba50
cam <cwan0148@student.monash.edu>
2023-10-03 23:49:35 +1100

    update styling

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,15 +5,15 @@ import styles from "./styles.module.css";
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-      <div className="container">
+      <div className={styles.buildContainer}>
         <p>Iteration 1 APK Build</p>
         <img src="/img/qr_it_1.svg" style={{ width: '80%' }} />
       </div>
-      <div className="container">
+      <div className={styles.buildContainer}>
         <p>Iteration 2 APK Build</p>
         <img src="/img/qr_it_2.png" style={{ width: '80%' }} />
       </div>
-      <div className="container">
+      <div className={styles.buildContainer}>
         <p>Iteration 3 APK Build</p>
         <img src="/img/settle-aid-icon.png" style={{ width: '80%' }} />
       </div>
