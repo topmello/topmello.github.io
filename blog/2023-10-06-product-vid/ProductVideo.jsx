336d1685442f9209fa2ab693d241c97f84a92182
jirathip-k <jkun0008@student.monash.edu>
2023-10-06 15:21:32 +1100

    fix width of product width

--- blog/2023-10-06-product-vid/ProductVideo.jsx
+++ blog/2023-10-06-product-vid/ProductVideo.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const ProductVideo = () => (
-  <video width="720" controls>
+  <video width="100%" controls>
     <source src="https://raw.githubusercontent.com/topmello/product-video/main/product-vid.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
